a9932d240b453980af1209107315d687dca7128b
Iñigo <inigo.gorosabel@gmail.com>
2020-03-10 00:03:14 +0100

    Actualizo OFW (4.19.0 -> 4.20.0)

--- ofw/base/OService.php
+++ ofw/base/OService.php
@@ -1,17 +1,12 @@
 <?php
-  class OService{
-    protected $controller = null;
+class OService {
+	protected $controller = null;
 
-    public final function setController($controller=null){
-      if (is_null($controller)){
-        $controller = new OController();
-        $controller->blankController();
-        
-      }
-      $this->controller = $controller;
-    }
+	public final function setController($controller) {
+		$this->controller = $controller;
+	}
 
-    public final function getController(){
-      return $this->controller;
-    }
-  }
+	public final function getController() {
+		return $this->controller;
+	}
+}
