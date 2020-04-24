b30def2321bbe87bd919f6ebcff48acdb12fe40f
Iñigo <inigo.gorosabel@gmail.com>
2020-04-24 18:52:43 +0200

    Actualizo plugin email (1.0.1 -> 1.1.1)

--- ofw/plugins/email/OEmail.php
+++ ofw/plugins/email/OEmail.php
@@ -1,143 +1,396 @@
-<?php
+<?php declare(strict_types=1);
 class OEmail {
-	private $debug        = false;
-	private $l            = null;
-	private $lang         = 'es';
-	private $recipients   = [];
-	private $subject      = '';
-	private $message      = '';
-	private $is_html      = true;
-	private $from         = '';
-	private $from_name    = null;
-	private $result_ok    = [];
-	private $result_error = [];
-	private $errors       = [
+	private bool    $debug        = false;
+	private ?OLog   $l            = null;
+	private string  $lang         = 'es';
+	private array   $recipients   = [];
+	private string  $subject      = '';
+	private string  $message      = '';
+	private bool    $is_html      = true;
+	private string  $from         = '';
+	private ?string $from_name    = null;
+	private array   $attachments  = [];
+	private string  $eol          = "\r\n";
+	private array   $result_ok    = [];
+	private array   $result_error = [];
+	private array   $errors       = [
 		'es' => ['NO_RECIPIENTS' => '¡No hay destinatarios!', 'ERROR_SENDING' => 'Error al enviar email a: '],
 		'es' => ['NO_RECIPIENTS' => 'There are no recipients!', 'ERROR_SENDING' => 'Error sending the email to: '],
 	];
-	
+
+	/**
+	 * Load debugger and application language on startup
+	 */
 	function __construct() {
 		global $core;
 		$this->debug = ($core->config->getLog('level') == 'ALL');
 		if ($this->debug) {
-			$this->l = new OLog();
+			$this->l = new OLog('OEmail');
 		}
 		$this->lang = $core->config->getLang();
 	}
 
-	private function log($str) {
+	/**
+	 * Logs internal information of the class
+	 *
+	 * @param string $str String to be logged
+	 *
+	 * @return void
+	 */
+	private function log(string $str): void {
 		if ($this->debug) {
 			$this->l->debug($str);
 		}
 	}
 
-	public function setRecipients($r) {
+	/**
+	 * Set email recipient list
+	 *
+	 * @param array $r Array of recipient emails
+	 *
+	 * @return void
+	 */
+	public function setRecipients(array $r): void {
 		$this->recipients = $r;
 	}
 
-	public function getRecipients() {
+	/**
+	 * Get email recipient list
+	 *
+	 * @return array Array of recipient emails
+	 */
+	public function getRecipients(): array {
 		return $this->recipients;
 	}
 
-	public function addRecipient($r) {
+	/**
+	 * Add recipient to the list
+	 *
+	 * @param string $r New recipients email
+	 *
+	 * @return void
+	 */
+	public function addRecipient(string $r): void {
 		array_push($this->recipients, $r);
 	}
 
-	public function setSubject($s) {
+	/**
+	 * Set emails subject
+	 *
+	 * @param string $s Emails subject
+	 *
+	 * @return void
+	 */
+	public function setSubject(string $s): void {
 		$this->subject = $s;
 	}
 
-	public function getSubject() {
+	/**
+	 * Get emails subject
+	 *
+	 * @return string Emails subject
+	 */
+	public function getSubject(): string {
 		return $this->subject;
 	}
 
-	public function setMessage($m) {
+	/**
+	 * Set emails message content (plain text or HTML)
+	 *
+	 * @param string $m Emails message content
+	 *
+	 * @return void
+	 */
+	public function setMessage(string $m): void {
 		$this->message = $m;
 	}
 
-	public function getMessage() {
+	/**
+	 * Get emails message content
+	 *
+	 * @return string Emails message content
+	 */
+	public function getMessage(): string {
 		return $this->message;
 	}
 
-	public function setIsHtml($ih) {
+	/**
+	 * Set if the email content is plain text or HTML
+	 *
+	 * @param bool $ih Emails message content is HTML
+	 *
+	 * @return void
+	 */
+	public function setIsHtml(bool $ih): void {
 		$this->is_html = $ih;
 	}
 
-	public function getIsHtml() {
+	/**
+	 * Get if the email content is HTML
+	 *
+	 * @return bool Emails content is HTML
+	 */
+	public function getIsHtml(): bool {
 		return $this->is_html;
 	}
 
-	public function setFrom($f, $name=null) {
+	/**
+	 * Set emails sender address and name
+	 *
+	 * @param string $f Senders email address
+	 *
+	 * @param string $name Senders full name
+	 *
+	 * @return void
+	 */
+	public function setFrom(string $f, ?string $name=null): void {
 		$this->from = $f;
 		if (!is_null($name)) {
 			$this->from_name = $name;
 		}
 	}
 
-	public function getFrom() {
+	/**
+	 * Get emails sender address
+	 *
+	 * @return string Senders email address
+	 */
+	public function getFrom(): string {
 		return $this->from;
 	}
 
-	public function setFromName($n) {
+	/**
+	 * Set senders full name
+	 *
+	 * @param string $n Senders full name
+	 *
+	 * @return void
+	 */
+	public function setFromName(string $n): void {
 		$this->from_name = $n;
 	}
 
-	public function getFromName() {
+	/**
+	 * Get senders full name
+	 *
+	 * @return string Senders full name
+	 */
+	public function getFromName(): string {
 		return $this->from_name;
 	}
 
-	public function setResultOk($ro) {
+	/**
+	 * Set list of filenames/paths to be attached to the email
+	 *
+	 * @param array $a List of filenames/paths to be attached
+	 *
+	 * @return void
+	 */
+	public function setAttachments(array $a): void {
+		$this->attachments = $a;
+	}
+
+	/**
+	 * Get list of filenames/paths to be attached to the email
+	 *
+	 * @return array List of filenames/paths to be attached
+	 */
+	public function getAttachments(): array {
+		return $this->attachments;
+	}
+
+	/**
+	 * Add filename/path to be attached to the email
+	 *
+	 * @param string $a Name/path of the file to be attached
+	 */
+	public function addAttachment(string $a): void {
+		array_push($this->attachments, $a);
+	}
+
+	/**
+	 * Set list of recipients that got the email correctly
+	 *
+	 * @param array $ro List of recipients
+	 *
+	 * @return void
+	 */
+	public function setResultOk(array $ro): void {
 		$this->result_ok = $ro;
 	}
 
-	public function getResultOk() {
+	/**
+	 * Get list of recipients that got the email correctly
+	 *
+	 * @return array List of recipients
+	 */
+	public function getResultOk(): array {
 		return $this->result_ok;
 	}
 
-	public function addResultOk($ro) {
+	/**
+	 * Add recipient to the list that got the email correctly
+	 *
+	 * @param string $ro Email address of the recipient
+	 *
+	 * @return void
+	 */
+	public function addResultOk(string $ro): void {
 		array_push($this->result_ok, $ro);
 	}
 
-	public function setResultError($re) {
+	/**
+	 * Set list of recipients that didn't get the email because of an error
+	 *
+	 * @param array List of recipients
+	 *
+	 * @return void
+	 */
+	public function setResultError(array $re): void {
 		$this->result_error = $re;
 	}
 
-	public function getResultError() {
+	/**
+	 * Get list of recipients that didn't get the email because of an error
+	 *
+	 * @return array List of recipients
+	 */
+	public function getResultError(): array {
 		return $this->result_error;
 	}
 
-	public function addResultError($re) {
+	/**
+	 * Add recipient to the list that didn't get the email because of an error
+	 *
+	 * @param string $ro Email address of the recipient
+	 *
+	 * @return void
+	 */
+	public function addResultError(string $re): void {
 		array_push($this->result_error, $re);
 	}
 
-	private function getErrorMessage($key) {
+	/**
+	 * Get localized error message
+	 *
+	 * @param string $key Key code of the requested message
+	 *
+	 * @return string Requested localized error message
+	 */
+	private function getErrorMessage(string $key): string {
 		return $this->errors[$this->lang][$key];
 	}
 
-	public function send() {
+	/**
+	 * Get email headers
+	 *
+	 * @param string $recipient Recipients email address
+	 *
+	 * @param string $separator String used to separate message content from the attachments
+	 *
+	 * @return string String of the required headers
+	 */
+	private function getHeaders(string $recipient, string $separator): string {
+		$headers = '';
+		if (count($this->attachments)==0){
+			// If is html add special headers
+			if ($this->is_html) {
+				$headers .= "Content-type: text/html; charset=\"utf-8\"".$this->eol;
+			}
+			else{
+				$headers .= "Content-Type: text/plain; charset=\"utf-8\"".$this->eol;
+			}
+		}
+		else{
+			$headers .= "Content-Type: multipart/mixed; boundary=\"".$separator."\"".$this->eol;
+		}
+		$headers .= "MIME-Version: 1.0\r\n";
+		$headers .= "To: ".$recipient."\r\n";
+		$headers .= "From: ".$this->from.( is_null($this->from_name) ? "" : "<".$this->from_name.">" ).$this->eol;
+
+		return $headers;
+	}
+
+	/**
+	 * Get email body
+	 *
+	 * @param string $separator String used to separate message content from the attachments
+	 *
+	 * @return string String with the bodys contentx
+	 */
+	private function getBody(string $separator): string {
+		$body = '';
+		if (count($this->attachments)>0){
+			$body .= "--" . $separator . $this->eol;
+			if ($this->is_html) {
+	    		$body .= "Content-Type: text/html; charset=\"utf-8\"" . $this->eol;
+	    	}
+	    	else{
+		    	$body .= "Content-Type: text/plain; charset=\"utf-8\"" . $this->eol;
+	    	}
+	    	$body .= "Content-Transfer-Encoding: 8bit" . $this->eol;
+	    }
+	    $body .= $this->message . $this->eol;
+
+	    return $body;
+	}
+
+	/**
+	 * Get attachment ready to be inserted into the emails body
+	 *
+	 * @param string $filename Name/path of the file to be attached
+	 *
+	 * @param string $separator String to separate email message content from the attachments
+	 *
+	 * @return string Attachment as string ready to be inserted into the email
+	 */
+	public function getAttachment(string $filename, string $separator): string {
+		$content = file_get_contents($filename);
+		$content = chunk_split(base64_encode($content));
+
+		$attachment = "--" . $separator . $this->eol;
+		$attachment .= "Content-Type: application/octet-stream; name=\"".basename($filename)."\"".$this->eol;
+		$attachment .= "Content-Transfer-Encoding: base64".$this->eol;
+		$attachment .= "Content-Disposition: attachment".$this->eol;
+		$attachment .= $content.$this->eol;
+		$attachment .= "--".$separator."--";
+
+		return $attachment;
+	}
+
+	/**
+	 * Send email
+	 *
+	 * @return array Status information array (ok/error) and error message (if any)
+	 */
+	public function send(): array {
 		$ret = ['status'=>'ok','mens'=>''];
 
 		// If there are no recipients, return error
-		if (count($this->getRecipients())==0) {
+		if (count($this->recipients)==0) {
 			$ret['status'] = 'error';
 			$ret['mens'] = $this->getErrorMessage('NO_RECIPIENTS');
 		}
 		else {
-			$list = $this->getRecipients();
-			$this->log('[OEmail] - Sending emails to '.count($list).' addresses');
+			$this->log('Sending emails to '.count($this->recipients).' addresses');
 
-			foreach ($list as $item) {
-				$headers = '';
-				// If is html add special headers
-				if ($this->getIsHtml()) {
-					$headers .= "MIME-Version: 1.0\r\n";
-					$headers .= "Content-type: text/html; charset=utf-8\r\n";
+			foreach ($this->recipients as $item) {
+				// A random hash will be necessary to send mixed content
+				$separator = md5(uniqid());
+
+				// Headers
+				$headers = $this->getHeaders($item, $separator);
+
+				// Body
+				$body = $this->getBody($separator);
+				
+				// Attachments
+				foreach ($this->attachments as $attachment) {
+					$body .= $this->getAttachment($attachment, $separator);
 				}
-				$headers .= "To: ".$item."\r\n";
-				$headers .= "From: ".$this->getFrom().( is_null($this->getFromName()) ? "" : "<".$this->getFromName().">" )."\r\n";
 
 				// Send the email
-				if (mail($item, $this->getSubject(), $this->getMessage(), $headers)) {
+				if (mail($item, $this->subject, $body, $headers)) {
 					$this->addResultOk($item);
 					$this->log('Email sent to: '.$item);
 				}
