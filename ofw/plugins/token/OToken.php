c01fec6a541d186301e6827f65d899e1e9627869
Iñigo <inigo.gorosabel@gmail.com>
2020-05-12 23:05:50 +0200

    Actualizo plugin token (1.0.2 -> 1.1.1)

--- ofw/plugins/token/OToken.php
+++ ofw/plugins/token/OToken.php
@@ -1,35 +1,68 @@
-<?php
+<?php declare(strict_types=1);
 class OToken {
-	private $secret = null;
-	private $params = [];
-	private $token = null;
+	private ?string $secret = null;
+	private array $params = [];
+	private ?string $token = null;
 
-	function __construct($secret) {
+	/**
+	 * Sets signature secret string on startup
+	 *
+	 * @param string $secret String used to sign the resulting token or check a tokens validity
+	 */
+	function __construct(string $secret) {
 		$this->secret = $secret;
 	}
 
-	public function setParams($params) {
+	/**
+	 * Set parameter array to be added to the token
+	 *
+	 * @param array $params Parameter array to be added to the token
+	 *
+	 * @return void
+	 */
+	public function setParams(array $params): void {
 		$this->params = $params;
 	}
 
-	public function addParam($key, $value) {
+	/**
+	 * Adds a parameter to the tokens parameters
+	 *
+	 * @param string $key Key code of the parameter
+	 *
+	 * @param string|int|float|bool $value Value of the parameter to be added
+	 *
+	 * @return void
+	 */
+	public function addParam(string $key, $value): void {
 		$this->params[$key] = $value;
 	}
 
-	public function getParams() {
+	/**
+	 * Gets list of tokens parameters
+	 *
+	 * @return array Array of tokens parameters
+	 */
+	public function getParams(): array {
 		return $this->params;
 	}
 
-	public function getParam($key) {
-		if (array_key_exists($key, $this->params)) {
-			return $this->params[$key];
-		}
-		else {
-			return false;
-		}
+	/**
+	 * Gets a single parameter of the token
+	 *
+	 * @param string $key Key code of the parameter to be retrieved
+	 *
+	 * @return string|int|float|bool Value of the requested parameter or null if not found
+	 */
+	public function getParam(string $key) {
+		return array_key_exists($key, $this->params) ? $this->params[$key] : null;
 	}
 
-	public function getToken() {
+	/**
+	 * Generates a new JWT token based on given parameters and signed with the given secret key
+	 *
+	 * @return string Generated JWT token
+	 */
+	public function getToken(): string {
 		if (!is_null($this->token)) {
 			return $this->token;
 		}
@@ -45,7 +78,14 @@ class OToken {
 		return $this->token;
 	}
 
-	public function checkToken($token) {
+	/**
+	 * Checks if a given token is valid
+	 *
+	 * @param string $token Token to be checked
+	 *
+	 * @return bool Token is valid or not
+	 */
+	public function checkToken(string $token): bool {
 		$pieces = explode('.', $token);
 		$header_64  = $pieces[0];
 		$payload_64 = $pieces[1];
