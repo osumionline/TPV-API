b4a6f6949f16f04d976eea1b630d30cd52641e65
Iñigo Gorosabel <inigo.gorosabel@gmail.com>
2023-01-12 13:26:23 +0100

    Quito funcion antigua del buscador de articulos

--- app/module/apiArticulos/apiArticulos.module.php
+++ app/module/apiArticulos/apiArticulos.module.php
@@ -5,7 +5,7 @@ namespace OsumiFramework\App\Module;
 use OsumiFramework\OFW\Routing\OModule;
 
 #[OModule(
-	actions: ['getStatistics', 'deleteArticulo', 'saveArticulo', 'loadArticulo', 'searchArticulos', 'getAccesosDirectos', 'asignarAccesoDirecto', 'deleteAccesoDirecto'],
+	actions: ['getStatistics', 'deleteArticulo', 'saveArticulo', 'loadArticulo', 'getAccesosDirectos', 'asignarAccesoDirecto', 'deleteAccesoDirecto'],
 	type: 'json',
 	prefix: '/api-articulos'
 )]
