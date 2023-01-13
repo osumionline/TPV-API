89be7d92fcd364bdcd792d27faf881935ad5a1cb
Iñigo Gorosabel <inigo.gorosabel@gmail.com>
2023-01-14 00:47:50 +0100

    Termino de documentar OTicketBai

--- ofw/plugins/ticketbai/OTicketBai.php
+++ ofw/plugins/ticketbai/OTicketBai.php
@@ -2,13 +2,11 @@
 
 namespace OsumiFramework\OFW\Plugins;
 
-use OsumiFramework\OFW\Tools\OTools;
-
 /**
- * Clase para realizar llamadas al servicio TicketBaiWS de Berein
+ * Clase para realizar llamadas al servicio TicketBaiWS de Berein ( https://ticketbaiws.eus/ )
  */
 class OTicketBai {
-	private string $url              = 'http://api.ticketbaiws.eus/';
+	private string $url              = 'https://api.ticketbaiws.eus/';
 	private string $token            = '';
 	private string $nif              = '';
 	private bool   $ssl_verification = false;
@@ -22,12 +20,36 @@ class OTicketBai {
 	}
 
 	/**
+	 * Método para activar / desactivar la verificación SSL
+	 *
+	 * @param bool $mode Modo de verificación SSL (activado / desactivado)
+	 *
+	 * @return void
+	 */
+	public function setSslVerification(bool $mode): void {
+		$this->ssl_verification = $mode;
+	}
+
+	/**
+	 * Método para activar / desactivar el modo debug
+	 *
+	 * @param bool $mode Modo debug (activado / desactivado)
+	 *
+	 * @return void
+	 */
+	public function setDebug(bool $mode): void {
+		$this->debug = $mode;
+	}
+
+	/**
 	 * Este método permite ver el estado del sistema y sirve para comprobar la conectividad.
 	 * En caso de que el certificado electrónico haya expirado, la licencia de ticketbai WS haya expirado o haya algún otro problema notificará un estado de ERROR.
 	 *
+	 * https://ticketbaiws.eus/es/documentacion-api/status-get/
+	 *
 	 * @return bool Estado del sistema
 	 */
-	public function check_status(): bool {
+	public function checkStatus(): bool {
 		$response = $this->callService('status', 'GET');		
 		return ($respuesta['result'] == 'OK');
 	}
@@ -41,11 +63,13 @@ class OTicketBai {
 	 * dado error, se ha de hacer reenviando de nuevo la factura que se desea corregir con el parámetro zuzendu a true. La serie y el número
 	 * de factura no pueden ser modificados, deben ser los mismos que contenía la factura original
 	 *
+	 * https://ticketbaiws.eus/es/documentacion-api/ticketbai-post/
+	 *
 	 * @param array $info_factura Parámetros de la factura
 	 *
 	 * @return string | array Array con información (huella, QR y url) en caso de éxito o mensaje de error
 	 */
-	public function nuevo_tbai(array $info_factura): string | array {
+	public function nuevoTbai(array $info_factura): string | array {
 		$response = $this->callService('tbai', 'POST', $info_factura);
 
 		if (!isset($response['result']) || $response['result'] == 'ERROR') {
@@ -60,11 +84,13 @@ class OTicketBai {
 	 * Este método permite convertir una o varias facturas simplificadas en una factura completa.
 	 * La factura completa deberá llevar su propio número. No se trata de una factura rectificativa aunque se trate como si fuera una de sustitución.
 	 *
+	 * https://ticketbaiws.eus/es/documentacion-api/tbai-completar-post/
+	 *
 	 * @param array $info_factura Parámetros de la factura
 	 *
 	 * @return string | array Array con información (huella, QR y url) en caso de éxito o mensaje de error
 	 */
-	public function tbai_completar(array $info_factura): string | array {
+	public function tbaiCompletar(array $info_factura): string | array {
 		$response = $this->callService('tbai-completar', 'POST', $info_factura);
 
 		if (!isset($response['result']) || $response['result'] == 'ERROR') {
@@ -78,11 +104,14 @@ class OTicketBai {
 	/**
 	 * Este método permite anular un TicketBAI enviado por error PERO no se podrá enviar otra factura con el mismo número.
 	 * Consulta las preguntas frecuentes de Batuz aquí: https://www.batuz.eus/es/preguntas-frecuentes?p_p_id=net_bizkaia_iybzwpfc_IYBZWPFCPortlet&p_p_lifecycle=2&p_p_state=normal&p_p_mode=view&p_p_resource_id=%2Fdescargar%2FpregFrec&p_p_cacheability=cacheLevelPage
+	 *
+	 * https://ticketbaiws.eus/es/documentacion-api/ticketbai-del/
+	 *
 	 * @param array $info_factura Parámetros de la factura (Serie y número)
 	 *
 	 * @return string | array Array vacío en caso de éxito o mensaje de error
 	 */
-	public function anula_tbai(array $info_factura): string | array {
+	public function anulaTbai(array $info_factura): string | array {
 		$response = $this->callService('tbai', 'DELETE', $info_factura);
 
 		if (!isset($response['result']) || $response['result'] == 'ERROR') {
@@ -96,11 +125,13 @@ class OTicketBai {
 	/**
 	 * Este método permite consultar la huella TBAI, el código QR y la URL de validación de una factura TicketBAI previamente generada.
 	 *
+	 * https://ticketbaiws.eus/es/documentacion-api/ticketbai-get/
+	 *
 	 * @param array $info_factura Parámetros de la factura (Serie y número)
 	 *
 	 * @return string | array Array con información (huella, QR y url) en caso de éxito o mensaje de error
 	 */
-	public function check_tbai(array $info_factura): string | array {
+	public function checkTbai(array $info_factura): string | array {
 		$response = $this->callService('tbai', 'GET', $info_factura);
 
 		if (!isset($response['result']) || $response['result'] == 'ERROR') {
@@ -115,11 +146,13 @@ class OTicketBai {
 	 * Este método descargarse los ficheros XML generados en la solicitud y la respuesta a la diputación correspondiente. Los ficheros
 	 * XML se devuelven encapsulados mediante Base64.
 	 *
+	 * https://ticketbaiws.eus/es/documentacion-api/ticketbai-xml-get/
+	 *
 	 * @param array $info_factura Parámetros de la factura (Serie y número)
 	 *
 	 * @return string | array Array con información (xml_request y xml_response) en caso de éxito o mensaje de error
 	 */
-	public function xml_tbai(array $info_factura): string | array {
+	public function xmlTbai(array $info_factura): string | array {
 		$response = $this->callService('tbai-xml', 'GET', $info_factura);
 
 		if (!isset($response['result']) || $response['result'] == 'ERROR') {
@@ -131,9 +164,15 @@ class OTicketBai {
 	}
 
 	/**
-	 * ?
+	 * Este método devuelve el listado de licencias contratadas
+	 *
+	 * https://ticketbaiws.eus/documentacion-api/licencias-get/
+	 *
+	 * @param array Array con el número de licencia
+	 *
+	 * @return string | array Array con el listado de licencias o mensaje de error
 	 */
-	public function get_licencias($datos) {
+	public function getLicencias(array $datos): string | array {
 		$response = $this->callService('licencias', 'GET', $datos);
 
 		if (!isset($response['result']) || $response['result'] == 'ERROR') {
@@ -145,9 +184,15 @@ class OTicketBai {
 	}
 
 	/**
-	 * ?
+	 * Este método permite crear una nueva licencia
+	 *
+	 * https://ticketbaiws.eus/documentacion-api/licencias-post/
+	 *
+	 * @param array $datos Datos para crear una nueva licencia
+	 *
+	 * @return string | array Array con el listado de ids de licencias o mensaje de error
 	 */
-	public function add_licencias($datos) {
+	public function addLicencias(array $datos): string | array {
 		$response = $this->callService('licencias', 'POST', $datos);
 
 		if (!isset($response['result']) || $response['result'] == 'ERROR') {
@@ -159,9 +204,15 @@ class OTicketBai {
 	}
 
 	/**
-	 * ?
+	 * Este método devuelve el listado de las empresas dadas de alta
+	 *
+	 * https://ticketbaiws.eus/documentacion-api/empresas-get/
+	 *
+	 * @param array $datos Datos con los que obtener el listado de empresas (id licencia, nif)
+	 *
+	 * @return string | array Array con el listado de empresas o mensaje de error
 	 */
-	public function get_empresas($datos) {
+	public function getEmpresas(array $datos): string | array {
 		$response = $this->callService('empresas', 'GET', $datos);
 
 		if (!isset($response['result']) || $response['result'] == 'ERROR') {
@@ -173,9 +224,15 @@ class OTicketBai {
 	}
 
 	/**
-	 * ?
+	 * Este método permite crear una nueva ficha de empresa para el envío de ticketbai
+	 *
+	 * https://ticketbaiws.eus/documentacion-api/empresas-post/
+	 *
+	 * @param array $datos Datos necesarios para crear una nueva empresa
+	 *
+	 * @return string | array Array con los datos de la nueva empresa o mensaje de error
 	 */
-	public function add_empresa($datos) {
+	public function addEmpresa(array $datos): string | array {
 		$response = $this->callService('empresas', 'POST', $datos);
 
 		if (!isset($response['result']) || $response['result'] == 'ERROR') {
@@ -187,9 +244,14 @@ class OTicketBai {
 	}
 
 	/**
-	 * ?
+	 * Este método devuelve el listado de los epígrafes de IAE para dar de alta cuentas de Ticketbai WS para
+	 * el envío de facturas de autónomos, comunidades de bienes o sociedades civiles
+	 *
+	 * https://ticketbaiws.eus/documentacion-api/epigrafes-get/
+	 *
+	 * @return string | array Array con los epígrafes o mensaje de error
 	 */
-	public function get_epigrafes() {
+	public function getEpigrafes() {
 		$response = $this->callService('epigrafes', 'GET', $datos);
 
 		if (!isset($response['result']) || $response['result'] == 'ERROR') {
