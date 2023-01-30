20be80bce1c8d58580165e7988cfe3bd7b68be07
Iñigo Gorosabel <inigo.gorosabel@gmail.com>
2023-01-30 17:52:18 +0100

    Sync stock: empiezo con funcion vacia

--- app/module/api/api.module.php
+++ app/module/api/api.module.php
@@ -5,7 +5,7 @@ namespace OsumiFramework\App\Module;
 use OsumiFramework\OFW\Routing\OModule;
 
 #[OModule(
-	actions: ['checkStart', 'saveInstallation', 'openBox', 'getTiposPago', 'deleteTipoPago', 'saveTipoPago', 'saveTipoPagoOrden', 'getSalidasCaja', 'saveSalidaCaja', 'deleteSalidaCaja', 'getCierreCaja', 'cerrarCaja', 'syncSale'],
+	actions: ['checkStart', 'saveInstallation', 'openBox', 'getTiposPago', 'deleteTipoPago', 'saveTipoPago', 'saveTipoPagoOrden', 'getSalidasCaja', 'saveSalidaCaja', 'deleteSalidaCaja', 'getCierreCaja', 'cerrarCaja', 'syncSale', 'syncStock'],
 	type: 'json',
 	prefix: '/api'
 )]
