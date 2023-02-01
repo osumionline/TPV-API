f56e48f7133a6dcf37e3b1f8453c3741b4d0d874
Iñigo Gorosabel <inigo.gorosabel@gmail.com>
2023-02-01 10:26:12 +0100

    Pedidos: la comprobacion de codigos de barras no estaba bien

--- app/module/apiCompras/actions/savePedido/savePedido.action.php
+++ app/module/apiCompras/actions/savePedido/savePedido.action.php
@@ -51,78 +51,84 @@ class savePedidoAction extends OAction {
 				}
 			}
 
-			// Guardo datos del pedido
-			$pedido->set('id_proveedor', $data->getIdProveedor());
-			$pedido->set('metodo_pago', $data->getIdMetodoPago());
-			$pedido->set('tipo', $data->getTipo());
-			$pedido->set('num', $data->getNum());
-			$pedido->set('importe', $data->getImporte());
-			$pedido->set('portes', $data->getPortes());
-			$pedido->set('descuento', $data->getDescuento());
-			if (!is_null($data->getFechaPago())) {
-				$pedido->set('fecha_pago', urldecode($data->getFechaPago()), '%e/%c/%Y');
+			if (count($cb_errors) > 0) {
+				$status = 'error';
 			}
-			else {
-				$pedido->set('fecha_pago', null);
-			}
-			if (!is_null($data->getFechaPedido())) {
-				$pedido->set('fecha_pedido', urldecode($data->getFechaPedido()), '%e/%c/%Y');
-			}
-			else {
-				$pedido->set('fecha_pedido', null);
-			}
-			$pedido->set('re', $data->getRe());
-			$pedido->set('europeo', $data->getUe());
-			$pedido->set('faltas', $data->getFaltas());
-			$pedido->set('recepcionado', $data->getRecepcionado());
-			if ($data->getRecepcionado() && is_null($pedido->get('fecha_recepcionado'))) {
-				$pedido->set('fecha_recepcionado', date('Y-m-d H:i:s', time()));
-			}
-			$pedido->set('observaciones', !is_null($data->getObservaciones()) ? urldecode($data->getObservaciones()) : null);
 
-			$pedido->save();
-			$data->setId($pedido->get('id'));
+			if ($status == 'ok') {
+				// Guardo datos del pedido
+				$pedido->set('id_proveedor', $data->getIdProveedor());
+				$pedido->set('metodo_pago', $data->getIdMetodoPago());
+				$pedido->set('tipo', $data->getTipo());
+				$pedido->set('num', $data->getNum());
+				$pedido->set('importe', $data->getImporte());
+				$pedido->set('portes', $data->getPortes());
+				$pedido->set('descuento', $data->getDescuento());
+				if (!is_null($data->getFechaPago())) {
+					$pedido->set('fecha_pago', urldecode($data->getFechaPago()), '%e/%c/%Y');
+				}
+				else {
+					$pedido->set('fecha_pago', null);
+				}
+				if (!is_null($data->getFechaPedido())) {
+					$pedido->set('fecha_pedido', urldecode($data->getFechaPedido()), '%e/%c/%Y');
+				}
+				else {
+					$pedido->set('fecha_pedido', null);
+				}
+				$pedido->set('re', $data->getRe());
+				$pedido->set('europeo', $data->getUe());
+				$pedido->set('faltas', $data->getFaltas());
+				$pedido->set('recepcionado', $data->getRecepcionado());
+				if ($data->getRecepcionado() && is_null($pedido->get('fecha_recepcionado'))) {
+					$pedido->set('fecha_recepcionado', date('Y-m-d H:i:s', time()));
+				}
+				$pedido->set('observaciones', !is_null($data->getObservaciones()) ? urldecode($data->getObservaciones()) : null);
 
-			if (!$recepcionado) {
-				// Borro líneas del pedido
-				$this->compras_service->borrarLineasPedido($pedido->get('id'));
+				$pedido->save();
+				$data->setId($pedido->get('id'));
 
-				// Guardo nuevas líneas del pedido
-				foreach ($data->getLineas() as $linea) {
-					$lp = new LineaPedido();
-					$lp->set('id_pedido', $pedido->get('id'));
-					$lp->set('id_articulo', $linea['idArticulo']);
-					$lp->set('nombre_articulo', urldecode($linea['nombreArticulo']));
-					$lp->set('codigo_barras', $linea['codBarras']);
-					$lp->set('unidades', $linea['unidades']);
-					$lp->set('palb', $linea['palb']);
-					$lp->set('pvp', $linea['pvp']);
-					$lp->set('iva', $linea['iva']);
-					$lp->set('re', $data->getRe() ? $linea['re'] : null);
-					$lp->set('descuento', $linea['descuento']);
-					$lp->save();
+				if (!$recepcionado) {
+					// Borro líneas del pedido
+					$this->compras_service->borrarLineasPedido($pedido->get('id'));
 
-					// Si el pedido está recepcionado
-					if ($pedido->get('recepcionado')) {
-						$articulo = new Articulo();
-						$articulo->find(['id' => $linea['idArticulo']]);
-						$articulo->set('stock', $articulo->get('stock') + $linea['unidades']);
-						$articulo->set('palb', $linea['palb']);
-						$articulo->set('pvp', $linea['pvp']);
-						$articulo->set('iva', $linea['iva']);
-						$articulo->set('re', $data->getRe() ? $linea['re'] : null);
-						$articulo->save();
+					// Guardo nuevas líneas del pedido
+					foreach ($data->getLineas() as $linea) {
+						$lp = new LineaPedido();
+						$lp->set('id_pedido', $pedido->get('id'));
+						$lp->set('id_articulo', $linea['idArticulo']);
+						$lp->set('nombre_articulo', urldecode($linea['nombreArticulo']));
+						$lp->set('codigo_barras', $linea['codBarras']);
+						$lp->set('unidades', $linea['unidades']);
+						$lp->set('palb', $linea['palb']);
+						$lp->set('pvp', $linea['pvp']);
+						$lp->set('iva', $linea['iva']);
+						$lp->set('re', $data->getRe() ? $linea['re'] : null);
+						$lp->set('descuento', $linea['descuento']);
+						$lp->save();
 
-						// Si viene un nuevo código de barras se lo creo
-						if (!is_null($linea['codBarras'])) {
-							$cb = new CodigoBarras();
-							$cb->set('id_articulo', $linea['idArticulo']);
-							$cb->set('codigo_barras', strval($linea['codBarras']));
-							$cb->set('por_defecto', false);
-							$cb->save();
+						// Si el pedido está recepcionado
+						if ($pedido->get('recepcionado')) {
+							$articulo = new Articulo();
+							$articulo->find(['id' => $linea['idArticulo']]);
+							$articulo->set('stock', $articulo->get('stock') + $linea['unidades']);
+							$articulo->set('palb', $linea['palb']);
+							$articulo->set('pvp', $linea['pvp']);
+							$articulo->set('iva', $linea['iva']);
+							$articulo->set('re', $data->getRe() ? $linea['re'] : null);
+							$articulo->save();
+
+							// Si viene un nuevo código de barras se lo creo
+							if (!is_null($linea['codBarras'])) {
+								$cb = new CodigoBarras();
+								$cb->set('id_articulo', $linea['idArticulo']);
+								$cb->set('codigo_barras', strval($linea['codBarras']));
+								$cb->set('por_defecto', false);
+								$cb->save();
+							}
+
+							// TODO: falta event log
 						}
-
-						// TODO: falta event log
 					}
 				}
 			}
