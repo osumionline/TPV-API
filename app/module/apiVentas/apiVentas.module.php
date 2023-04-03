2c87e0b1a7d7f3c8122ee067e9f4fa7a4a2f9df9
Iñigo Gorosabel <inigo.gorosabel@gmail.com>
2023-04-04 00:19:45 +0200

    Funcion temporal para enviar ventas a ticketbai

--- app/module/apiVentas/apiVentas.module.php
+++ app/module/apiVentas/apiVentas.module.php
@@ -5,7 +5,7 @@ namespace OsumiFramework\App\Module;
 use OsumiFramework\OFW\Routing\OModule;
 
 #[OModule(
-	actions: ['saveVenta', 'search', 'getHistorico', 'asignarTipoPago', 'sendTicket', 'printTicket', 'getLineasTicket'],
+	actions: ['saveVenta', 'search', 'getHistorico', 'asignarTipoPago', 'sendTicket', 'printTicket', 'getLineasTicket', 'sendTBai'],
 	type: 'json',
 	prefix: '/api-ventas'
 )]
