d1cb7a0a2fa72c333ba6bff03af7da56ddd55daa
Iñigo Gorosabel <inigo.gorosabel@gmail.com>
2026-05-06 23:10:51 +0200

    Metodo para obtener articulos de categorias

--- src/Routes/ApiCategorias.php
+++ src/Routes/ApiCategorias.php
@@ -4,7 +4,9 @@ namespace Osumi\OsumiFramework\Routes;
 
 use Osumi\OsumiFramework\Routing\ORoute;
 use Osumi\OsumiFramework\App\Module\ApiCategorias\GetCategorias\GetCategoriasComponent;
+use Osumi\OsumiFramework\App\Module\ApiCategorias\GetArticulosCategoria\GetArticulosCategoriaComponent;
 
 ORoute::prefix('/api-categorias', function() {
-  ORoute::post('/get-categorias', GetCategoriasComponent::class);
+  ORoute::post('/get-categorias',          GetCategoriasComponent::class);
+  ORoute::post('/get-articulos-categoria', GetArticulosCategoriaComponent::class);
 });
